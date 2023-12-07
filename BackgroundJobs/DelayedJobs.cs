0abfda51bdad7cdac57a74a99c7337df20752477
Ata Sahin <atasahin05@gmail.com>
2023-12-07 17:22:13 +0300

    Delayed Jobs Commit

---
+++ BackgroundJobs/DelayedJobs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Hangfire.Web.BackgroundJobs
+{
+    public class DelayedJobs
+    {
+        public static string AddWaterMarkJob(string filename,string watermarkText)
+        {
+
+           return Hangfire.BackgroundJob.Schedule(()=> ApplyWaterMark(filename,watermarkText),TimeSpan.FromSeconds(10));
+
+        }
+
+
+
+
+
+
+        public static void ApplyWaterMark(string filename,string watermarkText)
+        {
+            string path =Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/pictures",filename);
+
+
+            using (var bitmap=System.Drawing.Image.FromFile(path))
+            {
+               using(Bitmap tempBitmap=new Bitmap(bitmap.Width,bitmap.Height))
+                {
+                   using(Graphics grp=Graphics.FromImage(tempBitmap))
+                    {
+                     grp.DrawImage(bitmap,0,0);
+                        var font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold, GraphicsUnit.Pixel);
+                        var color = Color.FromArgb(128, 255, 255, 255);
+                        var brush = new SolidBrush(color);
+                        var point = new Point(0, 0);
+                        grp.DrawString(watermarkText, font, brush, point);
+                        tempBitmap.Save(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures", "watermark_" + filename));
+
+                   }
+               }
+            }
+
+
+
+
+        }
+
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Hangfire.Web.BackgroundJobs;
 using Hangfire.Web.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -40,6 +42,39 @@ namespace Hangfire.Web.Controllers
            //üye kayıt işlemi
            //yeni kullanıcının üyeliğini oluşturduktan sonra kullanıcıya mail gönderme işlemi
             FireAndForgetJobs.EmailSendToUserJob("1", "Merhaba, üyeliğiniz başarıyla oluşturuldu.");
+            return View();
+        }
+       
+   public IActionResult PictureSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PictureSave(IFormFile picture)
+        {
+            string newFileName = String.Empty;
+            if (picture != null && picture.Length > 0)
+            {
+                newFileName = Guid.NewGuid().ToString() + Path.GetExtension(picture.FileName);
+
+                var directory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures");
+
+                // Create the directory if it doesn't exist
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                var path = Path.Combine(directory, newFileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await picture.CopyToAsync(stream);
+                }
+
+                string jobID = BackgroundJobs.DelayedJobs.AddWaterMarkJob(newFileName, "www.mysite.com");
+            }
             return View();
         }
     }
